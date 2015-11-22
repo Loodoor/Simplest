29df1bea13b70b17a8a9301cce70f66a11970b82
Loodoor <urworld@outlook.fr>
2015-11-22 19:31:49 +0100

    Nouveau parser

--- main.py
+++ main.py
@@ -1,63 +1,86 @@
-﻿import re
+﻿class Struct:
+    def __init__(self, name: str):
+        self.name = name
 
 
-INT = "(?P<ival>[0-9]+)"
-FLOAT = "(?P<fval>[0-9]*\.[0-9]+)"
-STR = "(?P<sval>\".*\")"
-COMMENT = ";.+"
-FUNC_DECLARE = "func +(?P<func_name>[a-zA-Z0-9\?!]+) *<(?P<func_type>(int|float|str|void|empty))> *: *(?P<func_args>.+)*(\r|\n|\r\n)+(?P<func_body>(.|\r|\n|\r\n)+)*(\r|\n|\r\n)*(end +(?P=func_name)|ret +(?P<func_return>.+)) *"
-VAR_DECLARE = "(?P<var_declare_type>(dyn|cst)) +(?P<var_name>[a-zA-Z0-9\?!]+) +as +(int|float|str) *: *(?P<value>.+)"
-FUNC_BEGIN = "func +(?P<func_name>[a-zA-Z0-9\?!]+) *<(?P<func_type>(int|float|str|void|empty))> *: *(?P<func_args>.+)*"
-ARGS_CATCHER = "(?P<arg_name>.+) +as +(?P<arg_type>(int)|(float)|(str))"
-
-NEW_LINE = "\r\n"
-
-types = [STR, FLOAT, INT]
-tokens = [VAR_DECLARE, FUNC_DECLARE, COMMENT] + types
-semi = [FUNC_BEGIN]
+class Func:
+    def __init__(self, name: str, type_: Struct, args: list, corps: str):
+        self.name = name
+        self.type = type_
+        self.args = args
+        self.corps = corps
 
 
 def main():
-    stack = []
-    tmp = ""
+    stack = {}
+    ast = []
+    keywords = ['func', 'struct', 'dyn', 'cst', 'end']
+    comment = ';'
 
     while True:
-        founded = False
-        user_input = input("SPL:> ") if not tmp else tmp + NEW_LINE + input("SPL:: ")
+        indentation, spaces = 0, 0
+        chars_before, type_is_next, name_is_next, value_is_next = False, False, False, False
+        word = ""
 
-        for reg in tokens:
-            parsed = re.match(reg, user_input)
-            print("tokenzing ... (with : {})".format(reg))
+        user_input = input("SPL> ")
 
-            if parsed:
-                print("parsed !")
-                founded = True
-                ast = parsed.groupdict({})
-                if "value" in ast.keys() and ast["value"]:
-                    for type_ in types:
-                        val = re.match(type_, ast["value"])
-                        if val:
-                            ast["value"] = val.groupdict()
-                            break
-                if "func_args" in ast.keys() and ast["func_args"]:
-                    for tok in tokens:
-                        parsed2 = re.match(tok, ast['func_args'])
-                        if parsed2:
-                            ast['func_args'] = parsed2.groupdict()
-                            break
-                print(ast)
-                tmp = ""
-                stack.append(ast)
-        if not founded:
-            for reg in semi:
-                print("using semi expressions ...")
-                parsed = re.match(reg, user_input)
+        replaced = {'<': 0, '>': 0, ':': 0}
+        ui = list(user_input)
 
-                if parsed:
-                    print("parsed (with : {}) !".format(reg))
-                    tmp = user_input if not tmp else tmp + NEW_LINE + user_input
-                    break
+        for i in range(len(ui)):
+            c = ui[i]
+            if c in '<>:' and not replaced[c]:
+                replaced[c] = 1
+                ui[i] = ' '
+        user_input = ''.join(ui) + ' '
 
+        # traitement caractère par caractère
+        for i in range(len(user_input)):
+            char = user_input[i]
+
+            if char == comment:
+                break
+
+            if char.isspace():
+                # gestion de l'indentation
+                if not chars_before:
+                    spaces += 1
+                    if not spaces % 4:
+                        indentation += 1
+                        spaces = 0
+                # gestion des mots clés extraits
+                if word:
+                    if word in keywords:
+                        stack['indent'] = indentation
+                        stack['structure'] = word
+                        name_is_next = True
+                    elif word == "as":
+                        type_is_next = True
+                    # assignation autre qu'un mot clé
+                    else:
+                        if value_is_next:
+                            stack['value'] = word
+                            value_is_next = False
+
+                        if type_is_next:
+                            stack['type'] = word
+                            type_is_next = False
+                            if stack['structure'] in ('dyn', 'cst'):
+                                value_is_next = True
+
+                        if name_is_next:
+                            stack['name'] = word if not stack['name'] else stack['name']
+                            name_is_next = False
+                            if stack["structure"] == "func":
+                                type_is_next = True
+                    word = ""
+            else:
+                chars_before = True
+                word += char
+
+        ast.append(stack)
+        print(stack)
+        stack = {}
 
 if __name__ == '__main__':
     main()
